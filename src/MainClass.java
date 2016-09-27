10172e20e29fb723938a58dde5d967bae1cbc84f
fletchPL <wisniewski.maciek1@gmail.com>
2016-09-27 18:33:47 +0200

    add input file name

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
 
 public class MainClass {
 
 	public static String path="C://Users//Maciej//PAI//Zadanie1//file";
+	public static String userInput;
+	public static String[] fileName;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Type what file u wanna read separate ' ' ");
+		Scanner scan = new Scanner(System.in);
+		userInput = scan.nextLine(); // fileName from user 
+		fileName = userInput.split(" ");
 		
 		Plik pl = new Plik(path);
 		pl.getFileFromPath();
